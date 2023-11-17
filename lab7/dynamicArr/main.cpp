49afcb06ee34860af7c25c141ba103c637d4a3fb
Artem Galis <nowayamgs63@gmail.com>
2023-11-17 10:52:19 +0000

    task1 lab8

---
+++ lab7/dynamicArr/main.cpp
@@ -0,0 +1,13 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//    struct Employee{};
+//    Employee *empArr = new Employee;
+//    int numOfStaff;
+//    cout << "Enter the number of staff: " << endl;\
+//    cin >> numOfStaff;
+//    sizeof(empArr) = numOfStaff;
+//    return 0;
+//}
